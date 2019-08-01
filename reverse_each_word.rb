c3f5f9f56c98069f1e61b63da88b64ab22ca8d21
Benny Louie <bennylouie1412@gmail.com>
2019-08-01 18:56:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,10 @@ def reverse_each_word(sentence)
   reverse_sentence
 end
 
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  reverse_words = words.collect {|word| word.reverse!}
+  reverse_sentence = reverse_words.join(" ")
+  reverse_sentence
+end
+
